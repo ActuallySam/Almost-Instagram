0e1bfc42966f69dd5cff972a02b8fe36f1503f3a
ActuallySam <srivastava.samarth.btech2018@sitpune.edu.in>
2021-09-09 21:14:40 +0530

    Login Page Design

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
+// TODO: add to tailwind config
+// bg-blue-medium -> hex values
+// text-red-primary -> hex values
+// text-blue-medium -> hex values
+// text-gray-base -> hex values
+// border-gray-primary -> hex values
+
 module.exports = {
-    future: 
-        {
-            removeDeprecatedGapUtilities: true
-        }
+    future: {
+        removeDeprecatedGapUtilities: true
+    },
 };
